ae2f81126687b471590d5afaea0e302aa28e991e
Karan Desai <karan.desai@promactinfo.com>
2017-10-28 13:33:25 +0530

    Rearranged pokemons master branch

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -10,7 +10,7 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Charizard, Pikachu, Bulbasaur");
+            Console.WriteLine("Pikachu, Charizard, Bulbasaur");
             Console.Read();
         }
     }
